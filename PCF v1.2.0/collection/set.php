b3a929e55795ebe991698dfeb85122632d2e76b7
HallofFamer <worldoffame@hotmail.com>
2015-02-01 09:35:43 -0500

    Update set.php

--- PCF v1.2.0/collection/set.php
+++ PCF v1.2.0/collection/set.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace Resource\Collection;
-use Resource\Native\Objective;
+namespace Mysidia\Resource\Collection;
+use Mysidia\Resource\Native\Objective;
 
 /**
  * The abstract Set Class, extending from the abstract Collection Class and implements Settable Interface.
