6836487fe6654eefa8e691b84fb6c0d54973d734
HallofFamer <worldoffame@hotmail.com>
2015-02-01 09:40:44 -0500

    Update valuetreeset.php

--- PCF v1.2.0/collection/valuetreeset.php
+++ PCF v1.2.0/collection/valuetreeset.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace Resource\Collection;
-use Resource\Native\Objective;
+namespace Mysidia\Resource\Collection;
+use Mysidia\Resource\Native\Objective;
 
 /**
  * The ValueTreeSet Class, extending from the ValueMapSet Class.
