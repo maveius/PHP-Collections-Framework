f2c85ba36416041d5d8e9b5a598e6bcdb5fc2a6e
HallofFamer <worldoffame@hotmail.com>
2015-02-01 09:28:33 -0500

    Update keytreeset.php

--- PCF v1.2.0/collection/keytreeset.php
+++ PCF v1.2.0/collection/keytreeset.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace Resource\Collection;
-use Resource\Native\Objective;
+namespace Mysidia\Resource\Collection;
+use Mysidia\Resource\Native\Objective;
 
 /**
  * The KeyTreeSet Class, extending from the KeyMapSet Class and implementing the NavigableSettable Interface.
