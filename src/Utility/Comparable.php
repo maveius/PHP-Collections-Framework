1293da745ea0eb3f7674964ba0f93b659d9e9d27
Christopher Pitt <cgpitt@gmail.com>
2015-02-01 20:21:23 +0200

    Updated Utilities for PSR-2

--- src/Utility/Comparable.php
+++ src/Utility/Comparable.php
@@ -1,28 +1,28 @@
 <?php
 
 namespace Mysidia\Resource\Utility;
+
 use Mysidia\Resource\Native\Objective;
 
 /**
  * The Comparable Interface, it defines objects that can be compared with another.
  * It is a standard interface for objects that can be compared.
- * @category Resource
- * @package Utility
- * @author Ordland 
+ * @category  Resource
+ * @package   Utility
+ * @author    Ordland
  * @copyright Mysidia RPG, Inc
- * @link http://www.mysidiarpg.com 
+ * @link      http://www.mysidiarpg.com
  *
  */
- 
-interface Comparable{
-
-	/**
+interface Comparable
+{
+    /**
      * The compareTo method, compares this object with another.
-	 * @param Objective  $object
+     *
+     * @param Objective $object
+     *
      * @access public
      * @return Int
      */
     public function compareTo(Objective $object);
-
 }
-?>
