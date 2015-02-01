0102c69ef80fd8fa56fdec311e375461f7fce815
HallofFamer <worldoffame@hotmail.com>
2015-02-01 09:33:56 -0500

    Update priorityqueue.php

--- PCF v1.2.0/collection/priorityqueue.php
+++ PCF v1.2.0/collection/priorityqueue.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Resource\Collection;
-use Resource\Native\Objective;
-use Resource\Native\Arrays;
-use Resource\Utility\Comparative;
-use Resource\Exception\IllegalArgumentException;
+namespace Mysidia\Resource\Collection;
+use Mysidia\Resource\Native\Objective;
+use Mysidia\Resource\Native\Arrays;
+use Mysidia\Resource\Utility\Comparative;
+use Mysidia\Resource\Exception\IllegalArgumentException;
 
 /**
  * The PriorityQueue Class, extending from abstract Queue class.
