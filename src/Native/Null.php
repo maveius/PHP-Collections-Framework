b59edfbba7bb66bcebde6feb2be2149f83db9fa1
Christopher Pitt <cgpitt@gmail.com>
2015-02-03 23:54:09 +0200

    Tests for Native\Null

--- src/Native/Null.php
+++ src/Native/Null.php
@@ -4,40 +4,27 @@ namespace Mysidia\Resource\Native;
 
 /**
  * The Null Class, extending from root Object Class.
- * It defines a Null Object that does not do anything but to serve as placeholder or NulL Values.
+ *
+ * It defines a Null Object that does not do anything but to serve as
+ * placeholder or NulL Values.
+ *
  * @category  Resource
  * @package   Native
  * @author    Ordland
  * @copyright Mysidia RPG, Inc
  * @link      http://www.mysidiarpg.com
- *
  */
 class Null extends Object
 {
     /**
-     * The value property, which stores the primitive value for this Null object.
-     * @access private
-     * @var Null
-     */
-    private $value = null;
-
-    /**
      * The getValue method, returns the primitive null value.
+     *
      * @access public
-     * @return Null
+     *
+     * @return null
      */
     public function getValue()
     {
-        return $this->value;
-    }
-
-    /**
-     * Magic method __toString() for Null class, returns null.
-     * @access public
-     * @return String
-     */
-    public function __toString()
-    {
-        return "null";
+        return;
     }
 }
---
+++ tests/Native/NullTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mysidia\Resource\Test\Native;
+
+use Mysidia\Resource\Native\Null;
+use Mysidia\Resource\Test\Test;
+
+class NullTest extends Test
+{
+    /**
+     * @test
+     */
+    public function it_returns_a_value()
+    {
+        $null = new Null();
+
+        $this->assertNull($null->getValue());
+    }
+}
